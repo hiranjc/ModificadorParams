3743d2b2807a4d4d40b42e442eda6ff8ec4ba654
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:07:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ModificadorParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 3, 4);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
